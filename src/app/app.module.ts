6273d1344e409e1dc5214bf2339de92eb4763175
Maxime BRIAND <maxime.briand@soprasteria.com>
2020-11-12 21:13:42 +0100

    creation of the template, responsive for tablet and desktop

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
